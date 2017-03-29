76f2ee57d7bb14e53a975ab5b3ef091f007d6813
nyakuo <ynyakuo@gmail.com>
2017-03-29 14:14:54 +0900

    テストベンチ実装

---
+++ test_and.sv
@@ -0,0 +1,19 @@
+`include "and.sv"
+// Andのテストベンチ
+module TestAnd ();
+  reg a, b;
+  wire out;
+
+  And and1(.A(a), .B(b), .OUT(out));
+
+  localparam STEP = 50;
+
+  initial begin
+    $monitor($time, " a=%b b=%b out=%b", a, b, out);
+    a = 0; b = 0;
+    #STEP a = 0; b = 1;
+    #STEP a = 1; b = 0;
+    #STEP a = 1; b = 1;
+    $finish;
+  end
+endmodule
---
+++ test_and16.sv
@@ -0,0 +1,23 @@
+`include "and16.sv"
+// And16のテストベンチ
+module TestAnd16();
+  reg [15:0] a, b;
+  wire [15:0] out;
+
+
+  And16 a16(.A(a), .B(b), .OUT(out));
+
+  localparam STEP = 50;
+  reg [16:0] i, k;
+  initial begin
+    $monitor($time, " a=%16b b=%16b out=%16b", a, b, out);
+    for (i=0; i<=16'hffff; i=i+1) begin
+      a=i;
+      b=16'h00;
+      for (k=0; k<=16'hffff; k=k+1) begin
+        #STEP b=k;
+      end
+    end
+    $finish;
+  end
+endmodule
