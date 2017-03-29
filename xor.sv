433fba1b16c0c857e918306c4694fe262dc508e7
nyakuo <ynyakuo@gmail.com>
2017-03-29 20:31:54 +0900

    テストベンチの実装

--- xor.sv
+++ xor.sv
@@ -1,9 +1,16 @@
+`include "nand.sv"
+`include "or.sv"
+`include "and.sv"
+
+`ifndef __XOR__
+  `define __XOR__
 module Xor (
-  input a, b,
-  output out);
+  input A, B,
+  output OUT);
    wire                 w_nand, w_or;
 
-   Nand nand_1(.a(a), .b(b), .out(w_nand));
-   Or or_1(.a(a), .b(b), .out(w_or));
-   And and_1(.a(w_nand), .b(w_or), .out(out));
+   Nand nand_1(.A(A), .B(B), .OUT(W_nand));
+   Or or_1(.A(A), .B(B), .OUT(w_or));
+   And and_1(.A(W_nand), .B(w_or), .OUT(OUT));
 endmodule // xOr
+`endif
