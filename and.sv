8748eff9fb1ff7d7325b3466ff9c021e47c68a7e
nyakuo <ynyakuo@gmail.com>
2017-03-29 20:29:24 +0900

    コードの整形

--- and.sv
+++ and.sv
@@ -1,5 +1,8 @@
 `include "nand.sv"
 `include "not.sv"
+
+`ifndef __AND__
+  `define __AND__
 // a, b out
 // 0  0 0
 // 0  1 0
@@ -7,7 +10,9 @@
 // 1  1 1
 module And (input A, B,
                  output OUT);
-   wire                 tmp;
+   wire tmp;
+
    Nand nand_1(A, B, tmp);
    Not not_1(tmp, OUT);
 endmodule // And
+`endif
