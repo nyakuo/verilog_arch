42600723bf4dd7f1f9c934d6c7d628dfc393d74b
nyakuo <ynyakuo@gmail.com>
2017-04-04 20:35:07 +0900

    キャリーの接続間違いの修正

--- add16.sv
+++ add16.sv
@@ -8,15 +8,15 @@ module Add16(
   output wire [15:0] S,
   output wire C
 );
-  wire [15:0] tmp;
+  wire [14:0] tmp;
 
   HalfAdder ha1(.A(A[0]), .B(B[0]), .S(S[0]), .C(tmp[0]));
   generate
     genvar i;
-    for (i=0; i<15; i=i+1) begin
-      FullAdder fa(.A(A[i]), .B(B[i]), .X(tmp[i]), .S(S[i]), .C(tmp[i+1]));
+    for (i=1; i<15; i=i+1) begin
+      FullAdder fa(.A(A[i]), .B(B[i]), .X(tmp[i-1]), .S(S[i]), .C(tmp[i]));
     end
-    FullAdder fa15(.A(A[15]), .B(B[15]), .X(tmp[15]), .S(S[15]), .C(C));
+    FullAdder fa15(.A(A[15]), .B(B[15]), .X(tmp[14]), .S(S[15]), .C(C));
   endgenerate
 
 endmodule
